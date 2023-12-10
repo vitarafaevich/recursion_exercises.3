dd7ae865077415f971ba8e0007b81e956638cdec
vitarafavich <v.rafaevich@g.nsu.ru>
2023-12-10 19:20:03 +0700

    1-14 except 6, 9, 10-11

--- task_13.py
+++ task_13.py
@@ -1,13 +1,13 @@
-def odd_list(a):
-    if not a:
+def odd_list(a, n):
+    if n >= len(a):
         return []
+    if a[n] % 2 == 0:
+        return [n] + odd_list(a, n + 1)
     else:
-        if a[0] % 2 == 0:
-            return [a[0]] + odd_list(a[1:])
-        else:
-            return odd_list(a[1:])
+        return odd_list(a, n + 1)
 
 
 a = list(map(int, (input("enter the list items separated by a space ").split())))
+n = 0
 
-print(odd_list(a))
+print(odd_list(a, n))
