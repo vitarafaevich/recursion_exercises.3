55afbe45f2f870741b01d18b5be61f6a446e4313
vitarafavich <v.rafaevich@g.nsu.ru>
2023-12-10 19:30:29 +0700

    1-14 except 6, 9, 10-11

--- task_13.py
+++ task_13.py
@@ -1,3 +1,8 @@
+'''
+выводит индексы четных элементов
+'''
+
+
 def odd_list(a, n):
     if n >= len(a):
         return []
@@ -10,4 +15,22 @@ def odd_list(a, n):
 a = list(map(int, (input("enter the list items separated by a space ").split())))
 n = 0
 
-print(odd_list(a, n))
+print(odd_list(a))
+
+
+'''
+выводит четные элементы
+'''
+
+
+def odd_list(a):
+    if not a:
+        return []
+    if a[0] % 2 == 0:
+        return [a[0]] + odd_list(a[1:])
+    else:
+        return odd_list(a[1:])
+
+
+a = list(map(int, (input("enter the list items separated by a space ").split())))
+print(odd_list(a))
